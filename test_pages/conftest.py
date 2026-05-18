a997606209b17e9d81bbecc1736b7a739617154e
Batsheva <bat7cn@gmail.com>
2026-05-18 13:31:11 +0300

    Achieving test isolation using function-scoped fixtures

--- test_pages/conftest.py
+++ test_pages/conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from dotenv import load_dotenv
 load_dotenv()
 @pytest.fixture
-def driver():
+def driver(scope="function"):
     driver = webdriver.Chrome()
     driver.get("https://entry-point.me")
     yield driver
