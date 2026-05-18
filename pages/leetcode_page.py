9adc28e2ef88be04212d8eb9b83cf7c82961fe3b
Batsheva <bat7cn@gmail.com>
2026-05-18 14:31:38 +0300

    fix test solved_leetcode_count

--- pages/leetcode_page.py
+++ pages/leetcode_page.py
@@ -1,15 +1,18 @@
 from pages.hollder_driver import BaceHollderDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class PageLeetcode(BaceHollderDriver):
     def click_leetcode_buttom(self):
         self.driver.implicitly_wait(10)
-        
         leetcode_btn = self.driver.find_element(By.XPATH, "//span[text()='ליטקוד']")
         self.driver.execute_script("arguments[0].click();", leetcode_btn)
         
     def solved_leetcode_count(self):
-        self.driver.implicitly_wait(10)
-        return self.driver.find_element(By.XPATH,  "//span[.='29 הושלמו']").text
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(EC.visibility_of_element_located((By.XPATH,"//span[.='29 הושלמו']")))
+        return element.text
+    
 
