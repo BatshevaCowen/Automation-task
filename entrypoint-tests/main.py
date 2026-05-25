b712943ac109380cc1b1d3201368fee1c73dab80
Batsheva <bat7cn@gmail.com>
2026-05-25 19:45:57 +0300

    add uv

---
+++ entrypoint-tests/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from entrypoint-tests!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from test-entry-point!")
+
+
+if __name__ == "__main__":
+    main()
